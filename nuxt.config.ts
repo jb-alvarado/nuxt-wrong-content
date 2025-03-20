7382252b15e1642e7d5805ac870469997112d6ee
Jonathan Baecker <jonbae77@gmail.com>
2025-03-20 16:30:17 +0100

    cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,5 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    ssr: false,
-
-  //   nitro: {
-  //     devProxy: {
-  //         '/api': { target: 'http://127.0.0.1:8080' },
-  //     },
-  // },
-
     modules: ['@nuxt/eslint'],
 })
